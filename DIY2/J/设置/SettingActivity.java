43c81ba4e3b17c8facece35622249fa6c5157eae
zhxy-t <123500005+zhxy-t@users.noreply.github.com>
2023-01-28 18:54:38 +0800

    Update SettingActivity.java

--- DIY2/J/设置/SettingActivity.java
+++ DIY2/J/设置/SettingActivity.java
@@ -184,36 +184,13 @@ public class SettingActivity extends BaseActivity {
     
  public void onBackPressed() {
         if ((homeSourceKey != null && !homeSourceKey.equals(Hawk.get(HawkConfig.HOME_API, ""))) ||
-                !currentApi.equals(Hawk.get(HawkConfig.API_URL, "")) || !currentLive.equals(Hawk.get(HawkConfig.LIVE_URL, "")) ||
-                homeRec != Hawk.get(HawkConfig.HOME_REC, 0) ||
-                dnsOpt != Hawk.get(HawkConfig.DOH_URL, 0)) {
-            AppManager.getInstance().finishAllActivity();
-            if (currentApi.equals(Hawk.get(HawkConfig.API_URL, "")) & (currentLive.equals(Hawk.get(HawkConfig.LIVE_URL, "")))) {
-                Bundle bundle = new Bundle();
-                bundle.putBoolean("useCache", true);
-                jumpActivity(HomeActivity.class, bundle);
-            } else {
-                jumpActivity(HomeActivity.class);
-            }
-        } else {
-            super.onBackPressed();
-        }
-    }
-}
-    /*
-    @Override
-    public void onBackPressed() {
-        if ((homeSourceKey != null && !homeSourceKey.equals(Hawk.get(HawkConfig.HOME_API, ""))) ||
-            
-                !currentApi.equals(Hawk.get(HawkConfig.API_URL, "")) || 
+                !currentApi.equals(Hawk.get(HawkConfig.API_URL, "")) ||
                 !currentLive.equals(Hawk.get(HawkConfig.LIVE_URL, "")) ||
-            
                 !currentEPG.equals(Hawk.get(HawkConfig.EPG_URL, "")) ||
                 homeRec != Hawk.get(HawkConfig.HOME_REC, 0) ||
                 dnsOpt != Hawk.get(HawkConfig.DOH_URL, 0)) {
             AppManager.getInstance().finishAllActivity();
-            if (currentApi.equals(Hawk.get(HawkConfig.API_URL, "")) & currentEPG.equals(Hawk.get(HawkConfig.EPG_URL, "")) & (currentLive.equals(Hawk.get(HawkConfig.LIVE_URL, "")))) {
-   
+            if (currentApi.equals(Hawk.get(HawkConfig.API_URL, "")) & (currentLive.equals(Hawk.get(HawkConfig.LIVE_URL, "")) & (currentEPG.equals(Hawk.get(HawkConfig.EPG_URL, "")))) {
                 Bundle bundle = new Bundle();
                 bundle.putBoolean("useCache", true);
                 jumpActivity(HomeActivity.class, bundle);
@@ -225,4 +202,4 @@ public class SettingActivity extends BaseActivity {
         }
     }
 }
-*/
+   
